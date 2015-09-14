020039ebe32dbddce44ddb18d73181e41f3bbc1e
Asad Hasan <asad@dadesigners.com>
2015-09-14 13:12:54 -0500

    Start indexing app.

---
+++ elastic_indexing/build.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+var host = 'root:xyz786@office.uitoux.com';
+var port = '5988';
+var dbname = 'txprintco_dev_stage16';
+
+var nano = require("nano")('http://'+host+':'+port);
+var db = nano.db.use(dbname);
+
+var txprintco = {
+    host: host,
+    port: port,
+    database: db,
+    client: nano,
+    db: db,
+    design_doc: 'txprintco'
+};
+
+
+var txprintcoData = {
+	design_doc: txprintco.design_doc,
+	db: txprintco.db,
+	makeDataRequest: function(view, params, successCB, errorCB) {
+		this.db.view(this.design_doc, view, params, _.bind(this.handleCouchResponse, this, successCB, errorCB));
+	},
+	handleCouchResponse: function(successCB, errorCB, err, data) {
+		if(!err && _.isArray(data["rows"]) && data["rows"].length > 0) {
+			successCB(err, data["rows"]);
+		} else {
+			errorCB();
+		}
+	}
+};
+
+txprintcoData.makeDataRequest('filters-object',
+                {},
+                function(req, res) {
+                  console.log(res);
+                },
+                function() {
+                  console.log('Error contacting database.');
+                }
+);
