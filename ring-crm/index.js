6e768f39451ef131fd6db93f68270f2b3125a951
Asad Hasan <asad@uitoux.com>
2016-05-18 19:04:17 -0400

    RCSDK

--- ring-crm/index.js
+++ ring-crm/index.js
@@ -7,20 +7,4 @@ var rcsdk = new SDK({
     appSecret: 'B1ki_yEpRHOMQ8s7sexwCgjLXeipEQSk69JlKQ1cp_bQ'
 });
 
-rcsdk.platform()
-    .login({
-        username: '18007899736', // phone number in full format 
-        extension: '', // leave blank if direct number is used 
-        password: 'Mushtaq!1'
-    })
-    .then(function(response) {
-	  console.log('Login Successful!');
-          // your code here 
-	  var subscription = rcsdk.createSubscription();
-	  subscription.on(subscription.events.notification, function(msg) {
-    	    console.log(msg, msg.body);
-	  });
-    })
-    .catch(function(e) {
-        alert(e.message  || 'Server cannot authorize user');
-    });
+
